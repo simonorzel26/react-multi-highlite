4df51253223ff50d6583444c0b5da7db83bfdef8
Simon O <60504110+simonorzel26@users.noreply.github.com>
2024-07-21 14:10:55 +0200

    wip

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -17,26 +17,11 @@ export default function HomePage() {
         <pre className="p-4 bg-gray-800 text-white rounded mb-4">
           npm install react-multi-highlite
         </pre>
-        <h2 className="text-2xl font-bold mb-4">Usage</h2>
-        <p className="mb-4">
-          To use the <code>MultiHighLite</code> component, import it and pass the text you want to highlight along with an array of matcher objects. Each matcher object should contain the text to highlight and the class name to apply to the highlighted text.
-        </p>
-        <pre className="p-4 bg-gray-800 text-white rounded mb-4">
-          {`import MultiHighLite from 'react-multi-highlite';
-
-const text = "In software development, highlighting important pieces of information can greatly improve readability and comprehension.";
-const matchers = [
-  { text: "highlighting", classname: "bg-red-200 text-black" },
-  { text: "important", classname: "bg-green-300 text-black" },
-  { text: "information", classname: "bg-gray-600 text-white" },
-];
-
-<MultiHighLite matchers={matchers}>{text}</MultiHighLite>`}
-        </pre>
         <h2 className="text-2xl font-bold mb-4">Props</h2>
         <ul className="list-disc pl-6">
           <li><code>children</code>: The text to search for matches (required).</li>
           <li><code>matchers</code>: Array of matcher objects containing `text` and `classname` (required).</li>
+          <li><code>className</code>: The className of the entire line div (optional).</li>
           <li><code>caseSensitive</code> (optional): Whether the matching should be case-sensitive. Defaults to false.</li>
           <li><code>wrapperElement</code> (optional): Custom wrapper element for the highlighted text. This should be a function that returns a React element.</li>
         </ul>
@@ -56,22 +41,6 @@ const matchers = [
 
       <section className="w-full max-w-3xl">
         <CustomWrapperElement />
-      </section>
-
-      <section className="bg-white text-black p-6 rounded-lg shadow-lg w-full max-w-3xl">
-        <h2 className="text-2xl font-bold mb-4">API</h2>
-        <p className="mb-4">The <code>MultiHighLite</code> component accepts the following props:</p>
-        <ul className="list-disc pl-6 mb-4">
-          <li><code>children</code>: The text to search for matches (required).</li>
-          <li><code>matchers</code>: Array of matcher objects containing <code>text</code> and <code>classname</code> (required).
-            <ul className="list-disc pl-6">
-              <li><code>text</code>: The text to match and highlight.</li>
-              <li><code>classname</code>: The class name to apply to the highlighted text.</li>
-            </ul>
-          </li>
-          <li><code>caseSensitive</code> (optional): Whether the matching should be case-sensitive. Defaults to false.</li>
-          <li><code>wrapperElement</code> (optional): Custom wrapper element for the highlighted text. This should be a function that returns a React element.</li>
-        </ul>
       </section>
     </main>
   );
